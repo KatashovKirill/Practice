562e829c58f5e7f0f3e55ba2bbaf2dac42a4a18b
KatashovKirill <katashov.66@mail.ru>
2022-11-21 19:37:46 -0800

    Первые начинания

--- example-app/resources/views/funnycomicsland/main.blade.php
+++ example-app/resources/views/funnycomicsland/main.blade.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo asset('css/styleMain.css')?>" type="text/css"> 
+    <title>Funny Comics Land</title>
 </head>
 <body>
-    melon
+<header>
+        <div class="header">
+            <div class="menu">
+                <div class="O_Nas" role="link">
+                    <a href="#">О нас</a>
+                </div>
+                <div class="Katalog" role="link">
+                    <a href="#">Каталог</a>
+                </div>
+                <div class="Tovar" role="link">
+                    <a href="#">Товар</a>
+                </div>
+                <div class="Gde_Nas_Naiti" role="link">
+                    <a href="#">Где нас найти?</a>
+                </div>
+            </div>
+            <div class="singinup">
+                <button name="Singup">Регистрация</button>
+                <button name="Singin">Вход</button>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
