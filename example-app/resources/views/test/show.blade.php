95a262c0ec65c094e21fb097079f5bd1759c0584
KatashovKirill <katashov.66@mail.ru>
2022-11-23 00:22:18 -0800

    JJJ

---
+++ example-app/resources/views/test/show.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>My view</title> 
+</head> 
+<body>Первая переменная: {{$name}} Вторая переменная: {{$surname}}</body> 
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,5 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "main"]);
+ Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "main"]);
