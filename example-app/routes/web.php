f0fab740111a8d84b7786d27263b7b8dc6670323
KatashovKirill <katashov.66@mail.ru>
2022-11-26 00:08:17 -0800

    Всё починилось

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -7,6 +7,4 @@ Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::c
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
