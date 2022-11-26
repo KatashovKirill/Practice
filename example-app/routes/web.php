9256dae7d289ba70844e1691c8808448f3b813f9
KatashovKirill <katashov.66@mail.ru>
2022-11-25 22:32:38 -0800

    bootstrap

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
- Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "main"]);
+Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "main"]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
