cfc50c9b29da63f6cbd0b2b0e5792e406f55eaea
KatashovKirill <katashov.66@mail.ru>
2022-12-07 23:28:12 -0800

    Добавление товара

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "main"]);
+Route::get('funnycomicsland1', [App\Http\Controllers\funnycomicslandController::class, "main"]);
 
 Route::get('funnycomicsland', [App\Http\Controllers\funnycomicslandController::class, "test"]);
 
@@ -10,3 +10,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// Задание 8.4
+Route::get('zadanie84', [App\Http\Controllers\funnycomicslandController::class, "test"]);
